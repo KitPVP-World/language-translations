f2395e62773b1b05dff434352a86fd5bf53aab31
notstevy <notstevy@ultrabuildmc.de>
2025-03-31 16:57:23 +0200

    Add linux script to add the Gradle repository credentials to the environment  This script needs to be sourced using 'source lib/source_gradle.sh'. It works by reading from the global 'gradle.properties' file
    Signed-off-by: notstevy <notstevy@ultrabuildmc.de>

---
+++ lib/source_gradle.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status.
+# Treat unset variables as an error when substituting.
+set -eu
+
+gradle_home="${GRADLE_USER_HOME:-$HOME/.gradle}"
+gradle_properties_file="$gradle_home/gradle.properties"
+
+if [ ! -f "$gradle_properties_file" ]; then
+  echo "ERROR: File $gradle_properties_file does not exist. Cannot source gradle"
+  exit 1
+fi
+
+echo "Found 'gradle.properties' file at $gradle_properties_file"
+
+function prop {
+    grep "${1}" "${gradle_properties_file}" | cut -d'=' -f2
+}
+
+export repository_username="$(prop kitpvpWorldRepositoryUsername)"
+export repository_password="$(prop kitpvpWorldRepositoryPassword)"
+echo "Read successfully"
